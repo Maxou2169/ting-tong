78a1d89ef6258e7a5d64e55ac69531b6ea75cde2
Maxence BANEGAS <maxence.banegas@etu.univ-lyon1.fr>
2023-03-14 11:55:36 +0100

    Ajout de la documentation

--- src/includes/terrain.h
+++ src/includes/terrain.h
@@ -5,6 +5,10 @@
 #include "joueur.h"
 #include "balle.h"
 
+/**
+ * \brief Une classe Terrain qui possède deux joueurs et une balle
+*/
+
 class Terrain
 {
     private :
@@ -13,19 +17,53 @@ class Terrain
         Balle balle;
     
     public:
+
+    /**
+     * \brief Initialise un terrain avec deux joueur et une balle
+     * \param x : Le joueur 1
+     * \param y : Le joueur 2
+     * \param b : La balle
+    */
         Terrain(Joueur x, Joueur y, Balle b);
 
         ~Terrain();
 
+        /**
+         * \brief Retourne le joueur a
+        */
+
         Joueur get_joueur_a() const;
+
+        /**
+         * \brief Retourne le joueur b
+        */
 
         Joueur get_joueur_b() const;
 
+        /**
+         * \brief Retourne la balle du terrain
+        */
+
         Balle get_balle() const;
+
+        /**
+         * \brief Modifie le joueur a par le joueur j
+         * \param j : Le nouveau joueur
+        */
 
         void set_joueur_a(Joueur j);
 
+        /**
+         * \brief Modifie le joueur b par le joueur j
+         * \param j : Le nouveau joueur
+        */
+
         void set_joueur_b(Joueur j);
+
+        /**
+         * \brief Modifie la balle par la balle b
+         * \param b : La nouvelle balle
+        */
 
         void set_balle(Balle b);
 
