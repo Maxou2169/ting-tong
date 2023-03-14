8d993ef1a937c58ca2be380e746474715a45d219
PEGEOT ETHAN p2105678 <ethan.pegeot@etu.univ-lyon1.fr>
2023-03-14 10:15:06 +0000

    Update format.cpp

--- src/format.cpp
+++ src/format.cpp
@@ -1,37 +1,17 @@
 #include "includes/format.h"
 
-Format::Format() : un_jeu(false), trois_jeux(true), six_jeux(false) {}
-
-Format::Format(bool un, bool trois, bool six) : un_jeu(un), trois_jeux(trois), six_jeux(six) {}
+Format::Format() : nb_jeux(TROIS_JEUX) {}
+// TODO : Passer à un seul contructeur avec arguments par défauts
+Format::Format(Jeux nb_jeux) : nb_jeux(nb_jeux) {}
 
 Format::~Format() {}
 
-bool Format::get_un_jeu() const
+bool Format::get_jeu() const
 {
-    return this->un_jeu;
+    return this->nb_jeux;
 }
 
-bool Format::get_trois_jeux() const
+void Format::set_jeux(Jeux nb_jeux)
 {
-    return this->trois_jeux;
-}
-
-bool Format::get_six_jeux() const
-{
-    return this->six_jeux;
-}
-
-void Format::set_un_jeu(bool un)
-{
-    this->un_jeu = un;
-}
-
-void Format::set_trois_jeux(bool trois)
-{
-    this->trois_jeux = trois;
-}
-
-void Format::set_six_jeux(bool six)
-{
-    this->six_jeux = six;
+    this->nb_jeux = nb_jeux;
 }
