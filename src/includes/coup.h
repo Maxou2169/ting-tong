828bdd80df5956262fae1f3b9906a6019e228951
Maxence BANEGAS <maxence.banegas@etu.univ-lyon1.fr>
2023-03-14 15:22:07 +0100

    Ajout doc

--- src/includes/coup.h
+++ src/includes/coup.h
@@ -4,15 +4,33 @@
 #include "joueur.h"
 #include "balle.h"
 
+/**
+ * \brief La classe coup correspond au coup d'un joueur, que ce soit un coup droit ou un revers
+*/
+
 class Coup
 {
     private : 
         Joueur & joueur;
         Balle & balle;
+
+        /**
+         * \brief Vérifie si le joueur peut effectuer son coup ou non
+        */
     
         bool peut_faire_coup();
 
+        /**
+         * \brief Si le joueur peut faire un coup alors la fonction lui permet de le faire
+        */
+
         void faire_coup();
+
+        /**
+         * \brief Initialise un coup
+         * \param j : Un joueur
+         * \param b : Une balle
+        */
     public :
         
         Coup(Joueur & j, Balle &b);
