0dbec0c8f07a9bdd450254ba225a6452a49af06c
Ethan PEGEOT <git@coldsurikat.fr>
2023-04-03 22:41:00 +0200

    Init de srand dans le main

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,18 @@
 #include "includes/format.h"
 #include "includes/texte.h"
 
+#include <cstdlib>
+#include <ctime>
+
 bool run_test(void);
 
 int main(void)
 {
+    srand(time(NULL));
+
     run_test();
     Terrain t(Joueur("J1", Vec2(0,8), Score()), Joueur("J2", Vec2(0,-8), Score()), Balle());
     t.service();
-    //t.get_balle().set_traj(Vec2(0, 2));
-    Texte tx(t);
-    //tx.texte();
     Affichage a(t);
     a.render_loop();
     return (0);
