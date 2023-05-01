4218831a7db95a753ce1af120e846b17299e11f1
Maxence BANEGAS <maxence.banegas@etu.univ-lyon1.fr>
2023-05-01 14:44:56 +0200

    Ajout doc de texte.h

--- src/includes/texte.h
+++ src/includes/texte.h
@@ -5,16 +5,32 @@
 #include "balle.h"
 #include "terrain.h"
 
+/**
+ * \class Texte
+ * \brief Cette classe présente une versin intermediaire du jeu en mode texte
+*/
+
 class Texte
 {
 	private :
 		Terrain &terrain;
 
 	public :
+
+		/**
+		 * \brief Constructeur de la classe Texte
+		 * \param t : Le terrain
+		*/
 		Texte(Terrain &t);
 
+		/**
+		 * \brief Destructeur de la classe Texte
+		*/
 		~Texte();
 
+		/**
+		 * \brief Permet l'affichage textuel
+		*/
 		void texte();
 };
 
